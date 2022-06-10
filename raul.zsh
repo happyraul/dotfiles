e722f987b325928ad2b47e3ee56d4f8187d0bc40
Raul Taranu <597630+happyraul@users.noreply.github.com>
2022-06-10 17:01:18 +0200

    move raul theme

--- raul.zsh
+++ raul.zsh
@@ -1,12 +1,1 @@
-# prompt
-#PROMPT='[%{$fg[$NCOLOR]%}B%n%b%{$reset_colors}:%{$fg[red]%}%30<...<%~%<<%{$reset_color%}]%{#fg_bold[blue]%}%(!.#.ツ)%{$reset_color%} '
-#PROMPT='[%B%n%b:%F{red}%30<...<%~%<<%f]%B%F{blue}%(!.#.ツ)%f%b '
-PROMPT='[%B%n%b:%{$fg[red]%}%30<...<%~%<<%{$reset_color%}]%{$fg_bold[blue]%}%(!.#.ツ)%{$reset_color%} '
-RPROMPT='$(right_prompt)'
-
-# git theming
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[gray]%}%{$fg_no_bold[yellow]%}%B"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%b%{$fg_bold[gray]%}%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%} ✓ "
-ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg_bold[red]%}✗ "
-
+/home/raul/.zprompts/prompt_raul_setup
